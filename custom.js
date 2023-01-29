4d976d79e6572ba4ae3b6ce8349e22bc89b52327
Shamanesss <mentulbenodorna@gmail.com>
2023-01-29 10:48:14 +0100

    calculo del imc

--- custom.js
+++ custom.js
@@ -1,34 +1,38 @@
 let peso = document.getElementById('peso');
 let altura = document.getElementById('altura');
-let nombre =document.getElementById('nombre');
+let nombre =document.getElementById('name');
 let mensaje= document.getElementById('mensaje');
 let calcula = document.getElementById('calcula');
 
 calcula.addEventListener('click',imc);
 
 function imc(){
+	let identidad = nombre.value;
    let peso1= parseFloat(peso.value);
    let altura1= parseFloat(altura.value);
-    let resultado ='';
-    resultado =(peso1 / altura1*altura1);
-    if (imc < 16) {
-		grado = "Delgadez severa.";
-	} else if (imc >= 16 && imc <= 16.99) {
-		grado = "Delgadez moderada.";
-	} else if (imc >= 17 && imc <= 18.49) {
-		grado = "Delgadez aceptable.";
+	let imc = peso1 /Math.pow(altura1,2);
+	let grado = Math.round(imc * 100) / 100;
+    let resultado ="";
+	 //grado =(peso1 / altura1*altura1);
+     if (grado < 16) {
+		resultado =  " tu imc es de " + grado + " indica Delgadez severa.";
+	 } else if (imc >= 16 && imc <= 16.99) {
+		resultado = " tu imc es de " + grado +  " Delgadez moderada.";
+	 } else if (imc >= 17 && imc <= 18.49) {
+	 	resultado =  " tu imc es de " + grado + " Delgadez aceptable.";
 	} else if (imc >= 18.5 && imc <= 24.99) {
-		grado = "Normal.";
-	} else if (imc >= 25 && imc <= 29.99) {
-		grado = "Preobeso.";
-	} else if (imc >= 30 && imc <= 34.99) {
-		grado = "Obeso tipo I.";
-	} else if (imc >= 35 && imc <= 39.99) {
-		grado = "Obeso tipo II.";
-	} else if (imc > 40 ) {
-		grado = "Obeso tipo III.";
-	} 
-    mensaje.innerHTML= resultado;
-    console.log(resultado)
+	 	resultado =" tu imc es de " + grado +  " Normal.";
+	 } else if (imc >= 25 && imc <= 29.99) {
+		resultado = " tu imc es de " + grado + " Preobeso.";
+	 } else if (imc >= 30 && imc <= 34.99) {
+		resultado = " tu imc es de " + grado + " Obeso tipo I.";
+	 } else if (imc >= 35 && imc <= 39.99) {
+		resultado = " tu imc es de " + grado + " Obeso tipo II.";
+	 } else if (imc > 40 ) {
+		resultado = " tu imc es de " + grado + " Obeso tipo III.";
+	 } 
+	
+    mensaje.innerHTML= identidad + resultado;
+    console.log(grado);
 }
     
